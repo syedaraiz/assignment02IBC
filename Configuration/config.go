ae845aefe16854c9cdd862a50fe3f997c2e37d41
syedaraiz <45332906+syedaraiz@users.noreply.github.com>
2019-10-13 22:42:31 +0500

    2
    2

--- Configuration/config.go
+++ Configuration/config.go
@@ -1,4 +1,12 @@
 package config
 
 //MaxNumberOfUsersToInitiateBlockchain is Max Number Of Users To Initiate Blockchain
-var MaxNumberOfUsersToInitiateBlockchain = 4
+var MaxNumberOfUsersToInitiateBlockchain = 3
+//MiningReward is the Reward declared in start that all users will recieve
+var MiningReward = 10
+//MyCurrency ...
+var MyCurrency = "MyCoin"
+//MyCurrencyShortForm ...
+var MyCurrencyShortForm = "MC"
+//InitalSatoshiBalance ...
+var InitalSatoshiBalance = 100
